99741910d09da92fd722b06c3b8a34a86343a7ad
Zach Day <zachdayz@gmail.com>
2016-02-28 09:33:34 -0500

    More logic.

---
+++ game/app/client.py
@@ -0,0 +1,7 @@
+from socketIO_client import SocketIO
+
+class Connection:
+    def on_connect(self):
+        print('Established connection to server.')
+
+io = SocketIO('10.189.41.213', 7777, Connection)
