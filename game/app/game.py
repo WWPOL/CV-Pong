3e3a8475363226455708d01ef34e48d40e4ef786
philsiff <filman74@verizon.net>
2016-02-28 09:24:54 -0500

    Started Graphics

--- game/app/game.py
+++ game/app/game.py
@@ -1,7 +1,9 @@
 import pyglet
+from pyglet.gl import *
 from pyglet.window import key
-from ball import Ball
+from objects import Ball
 from physics import Vector
+from graphics import drawCircle
 
 window = pyglet.window.Window(width=1280, height=720)
 
@@ -14,6 +16,19 @@ def on_key_press(symbol, modifier):
 
 @window.event
 def on_draw():
-    window.clear()
+    glClear(GL_COLOR_BUFFER_BIT)
+    glLoadIdentity()
+    glTranslatef(0,0,-5)
+    drawCircle(0,0,0,1,30)
+    drawCircle(4,0,-3,1,30)
+
+@window.event
+def on_resize(width, height):
+    glViewport(0, 0, width, height)
+    glMatrixMode(GL_PROJECTION)
+    glLoadIdentity()
+    gluPerspective(65, width / float(height), .1, 1000)
+    glMatrixMode(GL_MODELVIEW)
+    return pyglet.event.EVENT_HANDLED
 
 pyglet.app.run()
